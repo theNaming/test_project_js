005c48c40f64ba8188c3f0202eb0efa8e3a1daa7
MacOS <k.dmitry.sales@yandex.ru>
2023-11-05 16:59:06 +0300

    Object ...destructire

--- js/script.js
+++ js/script.js
@@ -1,13 +1,41 @@
 /* eslint-disable */
 'use strict';
 
-function learnJS(lern, callback) {
-    console.log(`Я учу, ${lern}`);
-    callback();
+const customArray = {
+    name: 'car',
+    width: 192,
+    height: 10,
+    option: {
+        color: 'black',
+        gradient: 'purpul'
+    },
+    makeTest: function() {
+        console.log('test');
+    }
+};
+
+// delete customArray.makeTest;
+// console.log(customArray);
+
+// const {color, gradient} = customArray.option;
+
+// console.log(color, gradient);
+
+// customArray.makeTest();
+
+// console.log(Object.keys(customArray).length);
+
+let count = 0;
+
+for (let key in customArray) {
+    if (typeof(customArray[key]) === 'object') {
+        for (let i in customArray[key]) {
+            console.log(`Value ${i} is have properties ${customArray[key][i]}`);
+        }
+    } else {
+        console.log(`Value ${key} is have properties ${customArray[key]}`);
+        count++;
+    }
 }
 
-function done() {
-    console.log('Привет я коллбэК функция');
-}
-
-learnJS('JavaScript', done);
+console.log(count);
